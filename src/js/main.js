13eba1d145b0dd70f8d3ec59a1ecbbeee5546ea4
Yuri Renê Ferreira Rosino <yrosino@brtelecom.com.br>
2018-01-30 13:01:04 -0200

    fixed top mismatch

--- src/js/main.js
+++ src/js/main.js
@@ -16,30 +16,51 @@ const Colors = [
 const NavLinks = document.querySelectorAll('[rotate]')
 const Cube = document.querySelector('#cube')
 const SideWrappers = document.querySelectorAll('.side-wrapper')
+
+//testing
 window.cube = Cube
+
 const SideWidth = Array.prototype.slice.call(SideWrappers).reduce((prev, current) =>
   (prev.getBoundingClientRect().width > current.getBoundingClientRect().width) ? prev : current
 ).getBoundingClientRect().width
 
-const SidesDeg = {
-  front: 'rotateY(0deg)',         /* Y */
-  right: 'rotateY(-90deg)',     /* Y */
-  left: 'rotateY(90deg)',     /* Y */
-  back: 'rotateX(-180deg)', /* X */
-  top: 'rotateX(-90deg)', /* X */
-  bot: 'rotateX(90deg)' /* X */
+const SideHeight = Array.prototype.slice.call(SideWrappers).reduce((prev, current) =>
+  (prev.getBoundingClientRect().height > current.getBoundingClientRect().height) ? prev : current
+).getBoundingClientRect().height
+
+const Degrees = {
+  cube: {
+    front: 'rotateY(0deg)',         /* Y */
+    right: 'rotateY(-90deg)',     /* Y */
+    left: 'rotateY(90deg)',     /* Y */
+    back: 'rotateX(-180deg)', /* X */
+    top: 'rotateX(-90deg)', /* X */
+    bot: 'rotateX(90deg)' /* X */
+  },
+  sides: {
+    front: 'rotateY(0deg)',          /* Y */
+    right: 'rotateY(90deg)',       /* Y */
+    left: 'rotateY(-90deg)',     /* Y */
+    back: 'rotateX(180deg)',   /* X */
+    top: 'rotateX(90deg)',   /* X */
+    bot: 'rotateX(-90deg)' /* X */
+  }
 }
+
+//SideWrappers.forEach(el => el.style.minHeight = SideHeight + 'px')
 
 NavLinks.forEach(el => el.addEventListener('click', e => {
   e.preventDefault()
 
   const Side = el.getAttribute('rotate')
-  Cube.style.transform = SidesDeg[Side]
+  Cube.style.transform = Degrees.cube[Side]
 
-  const SideWrapper = document.querySelector(`#side-${Side}`)
-  SideWrapper.style.transform += 'translateZ(' + SideWidth / 2 + 'px)'
+  if (Side == 'back')
+    Cube.style.top = SideHeight + 'px'
+  else
+    Cube.style.top = '0'
 
-  console.log()
+  console.log(SideHeight, SideWidth)
 }))
 
 
